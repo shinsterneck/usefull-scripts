24d085a0dc2e8c2297bfc1a0a373cd3c890359cd
Shin Sterneck <shin@sterneck.asia>
2014-10-10 14:41:01 +0900

    added attach-and-email script

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 usefull-scripts
 ===============
 
-Usefull Scripts
+Usefull Scripts collection I use.
+
+
+attach-and-email
+----------------
+
+Contains a small script which helps sending files as attachment to an email via the command line using thunderbird as the email client. Should be easy to adapt to any other email client.
---
+++ attach-end-email/email.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Description: Open email client with supplied file as attachment
+# Author: (c) Shin Sterneck, shin at sterneck dot asia
+#
+if [[ $1 == "" ]]; then echo "$0 <filename>" ; exit 1 ; elif [[ $1 =~ ^/ ]]; then FN=$1 ; else FN="`pwd`/$1"; fi; 
+CMD="/usr/bin/thunderbird -compose \"attachment='file://$FN'\""
+eval $CMD > /dev/null 2>&1 &
