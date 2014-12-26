168f0325e7f7eafd277fd34711b0e0396ac4ada2
Shin Sterneck <shin@sterneck.asia>
2014-12-26 12:49:55 +0900

    made it more clean

--- attach-end-email/email.sh
+++ attach-end-email/email.sh
@@ -2,6 +2,7 @@
 # Description: Open email client with supplied file as attachment
 # Author: (c) Shin Sterneck, shin at sterneck dot asia
 #
-if [[ $1 == "" ]]; then echo "$0 <filename>" ; exit 1 ; elif [[ $1 =~ ^/ ]]; then FN=$1 ; else FN="`pwd`/$1"; fi
+if [[ $1 == "" ]]; then echo "$0 <filename>" ; exit 1 ; else FN=$(readlink -e $1) ; fi
 CMD="/usr/bin/thunderbird -compose \"attachment='file://$FN'\""
 eval $CMD > /dev/null 2>&1 &
+
